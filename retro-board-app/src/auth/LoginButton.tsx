414e5546d05c90f6a30bf0a804ed198972fc7d32
Antoine Jaussoin <antoine@jaussoin.com>
2020-02-29 13:06:31 +0000

    Avatar

--- retro-board-app/src/auth/LoginButton.tsx
+++ retro-board-app/src/auth/LoginButton.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useState, useRef, useContext } from 'react';
 import styled from 'styled-components';
-import Avatar from '@material-ui/core/Avatar';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
@@ -10,6 +9,7 @@ import LoginModal from './LoginModal';
 import useTranslation from '../translations/useTranslations';
 import { logout } from '../api';
 import UserContext from './Context';
+import Avatar from '../components/Avatar';
 
 const LoginButton = () => {
   const translations = useTranslation();
@@ -41,24 +41,12 @@ const LoginButton = () => {
   const user = useUser();
   if (user) {
     return (
-      <div>
+      <div style={{ position: 'relative' }}>
         <AvatarContainer onClick={openMenu} ref={menuAnchor}>
-          <Avatar alt={user.name} src={user.photo!} />
-          {user.name}
+          <Avatar user={user} />
+          <DisplayName>{user.name}</DisplayName>
         </AvatarContainer>
-        <Menu
-          anchorEl={menuAnchor.current}
-          anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-          open={menuOpen}
-          onClose={closeMenu}
-        >
+        <Menu anchorEl={menuAnchor.current} open={menuOpen} onClose={closeMenu}>
           <MenuItem onClick={handleLogout}>
             {translations.Header.logout}
           </MenuItem>
@@ -84,10 +72,21 @@ const LoginButton = () => {
 const AvatarContainer = styled.div`
   display: flex;
   align-items: center;
+  cursor: pointer;
 
   > *:first-child {
     margin-right: 10px;
   }
+`;
+
+const DisplayName = styled.div`
+  @media screen and (max-width: 500px) {
+    display: none;
+  }
+  max-width: 200px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 export default LoginButton;
