cf44b752a39c00116b075d0bfdab9dcfc22ebc44
Antoine Jaussoin <antoine@jaussoin.com>
2020-03-09 21:46:17 +0000

    Landing page WIP

--- retro-board-app/src/views/landing/LandingPage.tsx
+++ retro-board-app/src/views/landing/LandingPage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Paper, Typography, Card } from '@material-ui/core';
+import backgroundImage from './back.jpg';
+import styled from 'styled-components';
 
 const LandingPage = () => {
-  return <div>Landing Page</div>;
+  return (
+    <Container>
+      <Card style={{ margin: 0 }}>
+        <Typography variant="h2" style={{ color: 'black' }}>
+          Tired of post-its?
+        </Typography>
+      </Card>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  background-image: url(${backgroundImage});
+  width: 100%;
+  height: 100vh;
+  padding: 200px;
+`;
 
 export default LandingPage;
