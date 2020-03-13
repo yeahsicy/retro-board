af48ec42821876daf819948e10dbd0502f1d74e4
Antoine Jaussoin <antoine@jaussoin.com>
2020-03-13 17:49:28 +0000

    Adding Giphy support

--- retro-board-app/src/views/game/Post.tsx
+++ retro-board-app/src/views/game/Post.tsx
@@ -1,22 +1,32 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, useCallback, useMemo, useRef } from 'react';
 import styled from 'styled-components';
-import { Button, Typography, makeStyles, Tooltip } from '@material-ui/core';
+import {
+  Button,
+  Typography,
+  makeStyles,
+  Tooltip,
+  Popover,
+  Card,
+  CardActions,
+  CardContent,
+} from '@material-ui/core';
 import {
   ThumbUpOutlined,
   ThumbDownOutlined,
   DeleteForeverOutlined,
   FeedbackOutlined,
   Feedback,
+  Gif,
 } from '@material-ui/icons';
 import useTranslations from '../../translations';
 import EditableLabel from '../../components/EditableLabel';
 import { Palette } from '../../Theme';
 import { Post } from 'retro-board-common';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import { useUserPermissions } from './useUserPermissions';
 import { countVotes, enumerateVotes, VoteEnumeration } from './utils';
+import GiphySearchBox from 'react-giphy-searchbox';
+import useGiphy from '../../hooks/useGiphy';
+import config from '../../utils/getConfig';
 
 interface PostItemProps {
   post: Post;
@@ -25,6 +35,7 @@ interface PostItemProps {
   onDislike: () => void;
   onEdit: (content: string) => void;
   onEditAction: (action: string) => void;
+  onEditGiphy: (giphyId: string | null) => void;
   onDelete: () => void;
 }
 
@@ -44,6 +55,7 @@ const PostItem = ({
   onDislike,
   onEdit,
   onEditAction,
+  onEditGiphy,
   onDelete,
 }: PostItemProps) => {
   const {
@@ -56,7 +68,10 @@ const PostItem = ({
   } = useUserPermissions(post);
   const classes = useStyles();
   const { Actions: translations, Post: postTranslations } = useTranslations();
+  const giphyImageUrl = useGiphy(post.giphy);
+  const postElement = useRef(null);
   const [actionsToggled, setActionToggled] = useState(false);
+  const [showGiphy, setShowGiphy] = useState(false);
   const toggleActionPanel = useCallback(() => {
     setActionToggled(!actionsToggled);
   }, [actionsToggled]);
@@ -65,84 +80,140 @@ const PostItem = ({
   const upVoters = useMemo(() => enumerateVotes(post, 'like'), [post]);
   const downVoters = useMemo(() => enumerateVotes(post, 'dislike'), [post]);
   const displayAction = actionsToggled || !!post.action;
+  const handleShowGiphy = useCallback(() => {
+    setShowGiphy(true);
+  }, []);
+  const handleHideGiphy = useCallback(() => {
+    setShowGiphy(false);
+  }, []);
+  const handleChooseGiphy = useCallback(
+    (giphyItem: any) => {
+      onEditGiphy(giphyItem.id);
+    },
+    [onEditGiphy]
+  );
   return (
-    <PostCard>
-      <CardContent>
-        <Typography variant="body1">
-          <EditableLabel
-            readOnly={!canEdit}
-            value={post.content}
-            onChange={onEdit}
-            label="Post content"
-            multiline
-          />
-        </Typography>
-        {canShowAuthor && (
-          <AuthorContainer>
-            <Typography variant="caption" color="textSecondary" component="div">
-              {postTranslations.by}&nbsp;
-            </Typography>
-            <Typography variant="caption" color="textPrimary" component="div">
-              {post.user.name}
-            </Typography>
-          </AuthorContainer>
-        )}
-      </CardContent>
-      {displayAction && canCreateAction && (
-        <CardContent className={classes.actionContainer}>
-          <Typography variant="caption">{translations.title}:</Typography>
+    <>
+      <PostCard>
+        <CardContent>
           <Typography variant="body1">
             <EditableLabel
-              value={post.action || ''}
-              onChange={onEditAction}
-              label={translations.title}
-              focused={actionsToggled && !post.action}
+              readOnly={!canEdit}
+              value={post.content}
+              onChange={onEdit}
+              label="Post content"
               multiline
             />
           </Typography>
+          {canShowAuthor && (
+            <AuthorContainer>
+              <Typography
+                variant="caption"
+                color="textSecondary"
+                component="div"
+              >
+                {postTranslations.by}&nbsp;
+              </Typography>
+              <Typography variant="caption" color="textPrimary" component="div">
+                {post.user.name}
+              </Typography>
+            </AuthorContainer>
+          )}
+          {giphyImageUrl && (
+            <GiphyContainer>
+              <img src={giphyImageUrl} alt="Giphy" height="200px" />
+            </GiphyContainer>
+          )}
         </CardContent>
-      )}
-      <CardActions style={{ backgroundColor: color }}>
-        <VoteButton
-          voters={upVoters}
-          canVote={canUpVote}
-          count={upVotes}
-          icon={<ThumbUpOutlined style={{ color: Palette.positive }} />}
-          onClick={onLike}
-          showTooltip={canShowAuthor}
-          ariaLabel="Like"
-        />
-        <VoteButton
-          voters={downVoters}
-          canVote={canDownVote}
-          count={downVotes}
-          icon={<ThumbDownOutlined style={{ color: Palette.negative }} />}
-          onClick={onDislike}
-          showTooltip={canShowAuthor}
-          ariaLabel="Dislike"
-        />
-        {canDelete && (
-          <Button onClick={onDelete} aria-label="Delete" tabIndex={-1}>
-            <DeleteForeverOutlined style={{ color: Palette.negative }} />
-          </Button>
+        {displayAction && canCreateAction && (
+          <CardContent className={classes.actionContainer}>
+            <Typography variant="caption">{translations.title}:</Typography>
+            <Typography variant="body1">
+              <EditableLabel
+                value={post.action || ''}
+                onChange={onEditAction}
+                label={translations.title}
+                focused={actionsToggled && !post.action}
+                multiline
+              />
+            </Typography>
+          </CardContent>
         )}
-        {canCreateAction && (
-          <Button
-            onClick={toggleActionPanel}
-            aria-label={translations.label}
-            title={translations.tooltip}
-            disabled={!!post.action}
-            tabIndex={-1}
-          >
-            {post.action ? (
-              <Feedback className={classes.actionIcon} />
-            ) : (
-              <FeedbackOutlined className={classes.actionIcon} />
-            )}
-          </Button>
-        )}
-      </CardActions>
-    </PostCard>
+        <CardActions style={{ backgroundColor: color }}>
+          <VoteButton
+            voters={upVoters}
+            canVote={canUpVote}
+            count={upVotes}
+            icon={<ThumbUpOutlined style={{ color: Palette.positive }} />}
+            onClick={onLike}
+            showTooltip={canShowAuthor}
+            ariaLabel="Like"
+          />
+          <VoteButton
+            voters={downVoters}
+            canVote={canDownVote}
+            count={downVotes}
+            icon={<ThumbDownOutlined style={{ color: Palette.negative }} />}
+            onClick={onDislike}
+            showTooltip={canShowAuthor}
+            ariaLabel="Dislike"
+          />
+          {canDelete && (
+            <Button onClick={onDelete} aria-label="Delete" tabIndex={-1}>
+              <DeleteForeverOutlined style={{ color: Palette.negative }} />
+            </Button>
+          )}
+          {canCreateAction && (
+            <Button
+              onClick={toggleActionPanel}
+              aria-label={translations.label}
+              title={translations.tooltip}
+              disabled={!!post.action}
+              tabIndex={-1}
+            >
+              {post.action ? (
+                <Feedback className={classes.actionIcon} />
+              ) : (
+                <FeedbackOutlined className={classes.actionIcon} />
+              )}
+            </Button>
+          )}
+          {canEdit && config.hasGiphy && (
+            <Button
+              onClick={handleShowGiphy}
+              aria-label="Ghipfy"
+              tabIndex={-1}
+              ref={postElement}
+            >
+              <Gif />
+            </Button>
+          )}
+        </CardActions>
+      </PostCard>
+      <Popover
+        open={showGiphy}
+        anchorEl={postElement.current}
+        onClose={handleHideGiphy}
+        onEscapeKeyDown={handleHideGiphy}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <Card>
+          <CardContent>
+            <GiphySearchBox
+              apiKey="cnX8V9qGT801TRFb1mOz8dj0N5mKSe0U"
+              onSelect={handleChooseGiphy}
+            />
+          </CardContent>
+        </Card>
+      </Popover>
+    </>
   );
 };
 
@@ -214,6 +285,17 @@ const AuthorContainer = styled.div`
   margin-top: -10px;
   top: 10px;
   right: -5px;
+`;
+
+const GiphyContainer = styled.div`
+  img {
+    width: 100%;
+    object-fit: fit;
+    height: 100%;
+  }
+
+  margin: -20px;
+  margin-top: 50px;
 `;
 
 export default PostItem;
