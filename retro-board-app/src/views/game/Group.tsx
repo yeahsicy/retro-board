f4633280c9c6559ca8698fa9d66fba1c8755d4fe
Antoine Jaussoin <antoine@jaussoin.com>
2020-03-21 15:47:01 +0000

    Container

--- retro-board-app/src/views/game/Group.tsx
+++ retro-board-app/src/views/game/Group.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { PostGroup } from 'retro-board-common';
+import styled from 'styled-components';
 
 interface GroupProps {
   group: PostGroup;
 }
 
 const Group = ({ group }: GroupProps) => {
-  return <div>{group.label}</div>;
+  return <GroupContainer>{group.label}</GroupContainer>;
 };
+
+const GroupContainer = styled.div`
+  padding: 20px;
+  border: 2px dashed lightgray;
+  border-radius: 10px;
+  margin: 20px 0;
+`;
 
 export default Group;
